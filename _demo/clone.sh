ad7817634b10a56ea56c658105c6ca533d9becd5
samwhelp <samwhelp@users.noreply.github.com>
2023-03-25 01:54:06 +0800

    fix

---
+++ _demo/clone.sh
@@ -0,0 +1,86 @@
+
+##
+## https://github.com/samwhelp/gruvbox-theme-collection
+##
+
+git clone -b gh-pages git@github.com:samwhelp/gruvbox-theme-collection.git gruvbox-theme-collection-gh-pages
+
+cd gruvbox-theme-collection-gh-pages
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+
+git clone -b all git@github.com:samwhelp/gruvbox-theme-collection.git gruvbox-theme-collection-all
+
+cd gruvbox-theme-collection-all
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b gtk-theme-gruvbox git@github.com:samwhelp/gruvbox-theme-collection.git gruvbox-theme-collection-gtk-theme-gruvbox
+
+cd gruvbox-theme-collection-gtk-theme-gruvbox
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b gtk-theme-gruvbox-dark git@github.com:samwhelp/gruvbox-theme-collection.git gruvbox-theme-collection-gtk-theme-gruvbox-dark
+
+cd gruvbox-theme-collection-gtk-theme-gruvbox-dark
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b icon-theme-gruvbox-dark git@github.com:samwhelp/gruvbox-theme-collection.git gruvbox-theme-collection-icon-theme-gruvbox-dark
+
+cd gruvbox-theme-collection-icon-theme-gruvbox-dark
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b vim-theme-gruvbox git@github.com:samwhelp/gruvbox-theme-collection.git gruvbox-theme-collection-vim-theme-gruvbox
+
+cd gruvbox-theme-collection-vim-theme-gruvbox
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
+
+
+
+
+git clone -b nvim-theme-gruvbox git@github.com:samwhelp/gruvbox-theme-collection.git gruvbox-theme-collection-nvim-theme-gruvbox
+
+cd gruvbox-theme-collection-nvim-theme-gruvbox
+
+git config user.name samwhelp
+git config user.email samwhelp@users.noreply.github.com
+
+cd "$OLDPWD"
